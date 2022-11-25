1bb2b20be5223682428d3e2303e078044bf18909
evanqian2175 <evanqian2175@gmail.com>
2022-11-25 21:46:01 +1300

    say command + test environment

---
+++ Commands/Public/say.js
@@ -0,0 +1,39 @@
+const {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  EmbedBuilder,
+  Client,
+} = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('say')
+    .setDescription(`Says the provided text.`)
+    .addStringOption((options) =>
+      options
+        .setName('message')
+        .setDescription('The message the bot is going to say.')
+        .setRequired(true)
+    ),
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const { options } = interaction;
+
+    const message = options.getString('message');
+
+    if (!message)
+      return interaction.reply({
+        content: 'Please add a message.',
+        ephemeral: true,
+      });
+    else {
+      interaction.reply({
+        content: `${message}`,
+      });
+    }
+  },
+};
