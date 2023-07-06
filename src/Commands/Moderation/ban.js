936e49e1bf63d16541eaa29a14e70004ae14264d
Voxlinou <71925280+voxlinou1@users.noreply.github.com>
2023-07-06 02:05:56 +0100

    Update ban.js

--- src/Commands/Moderation/ban.js
+++ src/Commands/Moderation/ban.js
@@ -14,13 +14,13 @@ module.exports = {
     .setDescription(`Bans a user.`)
     .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
     .setDMPermission(false)
-    .addUserOption((option) =>
+    .addUserOption(option =>
       option
         .setName('input')
         .setDescription('Select the target member.')
         .setRequired(true)
     )
-    .addStringOption((option) =>
+    .addStringOption(option =>
       option
         .setName('reason')
         .setDescription('Provide a reason for this ban.')
